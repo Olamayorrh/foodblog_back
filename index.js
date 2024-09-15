9268e8ca9870465e1628544c5da61a5b8532c6a1
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-09-15 16:48:05 +0100

    updated

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use(cors({origin:"*", optionsSuccessStatus:200}));
 app.use("/", routes);
 app.use(express.urlencoded({extended:false}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 // route
 
